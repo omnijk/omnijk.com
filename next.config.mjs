ef9fdd1fbfa1fe2e378ee0e291d948a4993239d0
omnijk <2095272376@qq.com>
2026-05-23 09:44:26 +0800

    fix：修改next配置文件，解决<Image>组件不允许加载未经授权的域名图片问题

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,23 @@ import path from "node:path";
 const nextConfig = {
   // 生成 standalone 输出，便于在无完整构建环境的服务器上运行
   output: "standalone",
+  // next的<Image>组件不允许加载未经授权的域名图片
+  // 导致报错，GET _next/image?url=%2Fimages%2Fhome.png 400 (Bad Request)
   images: {
     remotePatterns: [
       { hostname: "img.clerk.com" },
       { hostname: "cdn.sanity.io" },
       { hostname: "i.scdn.co" },
+      {
+        protocol: 'https', 
+        hostname: 'omnijk.com',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.omnijk.com',
+        pathname: '/images/**',
+      },
     ],
   },
   pageExtensions: ["js", "jsx", "mdx"],
